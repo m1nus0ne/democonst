3e3c2b31d3f66bfdc317e4e4d946353db5819446
m1nus0ne <Voldemar7455@Gmail.com>
2021-01-02 23:03:06 +0500

    added const.py

--- const.py
+++ const.py
@@ -1,1 +1,19 @@
-import PIL as p
+from PIL import Image
+from os import listdir,chdir
+from sys import exit
+from time import sleep
+
+
+chdir('pic')
+if len(listdir())!=1:
+	print('Only 1 file expected')
+	sleep(2)
+	exit()
+
+pic=listdir()[0]
+
+
+im=Image.open(pic)
+im.
+
+
