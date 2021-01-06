4c8d2d74dda28310b28ac3273f35c3bcf1c45e67
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2021-01-06 13:21:56 +0500

    OMG it's working

--- const.py
+++ const.py
@@ -1,34 +1,39 @@
-from PIL import Image,ImageDraw
-from os import listdir,chdir
+from PIL import Image, ImageDraw
+from os import listdir, chdir
 from sys import exit
 from time import sleep
-#Часто работаем с кортежами, пишем нужную функцию округления
+from constants import *
+
+
+# Часто работаем с кортежами, пишем нужную функцию округления
 def rround(*args):
-	arr = [round(i) for i in args]
-	return tuple(arr)
+    return tuple(map(round, args))
 
-#Подсос избражения
+
+# Подсос избражения
 chdir('pic')
-if len(listdir())!=1:
-	print('Only 1 file expected')
-	sleep(2)
-	exit()
-pic=listdir()[0]
-im =Image.open(pic)
-#ДА
-WIDTH=im.size[1]
-HEIGHT=im.size[0]
-BORDER=20
-#Вкладываем основную пикчу в рамку, рамку+пикчу на демотиватор с надписью
+if len(listdir()) != 1:
+    print('Only 1 file expected')
+    sleep(2)
+    exit()
+pic = listdir()[0]
 
-b_im=Image.new('RGB',(HEIGHT+BORDER,WIDTH+BORDER),(0,0,0))
-draw = ImageDraw.Draw(b_im)
-draw.rectangle((0,0,HEIGHT+BORDER,WIDTH+BORDER),None,(255,255,255),BORDER//4)# TODO: Попавить расчеты
-b_im.paste(im,(int(0.5*BORDER),int(BORDER*0.5)))
+im: Image.Image = Image.open(pic)
+"""
+Пояснение: автозаполнение в pycharm работает нормально, но по какой-то причине объект, создаваемый ф-цией Image.open(),
+не воспринимается как объект класса Image.Image и если использовать синтаксис как показвно выше, то pycharm будет нормально с ним работать
+P.S. с Image.new() такого не происходит
+"""
+height = im.size[0]
+width = im.size[1]
+# Вкладываем основную пикчу в рамку, рамку+пикчу на демотиватор с надписью
 
+background = Image.new('RGB', (height + BORDER, width + BORDER), (0, 0, 0)) # TODO: здесь
+draw = ImageDraw.Draw(background)
+draw.rectangle((0, 0, height + BORDER, width + BORDER), None, WHITE, LINE_SIZE)
+background.paste(im, (int(0.5 * BORDER), int(BORDER * 0.5))) # TODO: и здесь заменить BORDER через LINE_SIZE
 
-f_im = Image.new('RGB', rround((HEIGHT+BORDER)*1.2,(WIDTH+BORDER)*1.8),(0,0,0))
-f_im.paste(b_im,rround((0.8*HEIGHT-BORDER)/8,(0.2*WIDTH-BORDER)/8))
-f_im.show()
-
-
+result_image = Image.new('RGB', (height + 2 * IMAGE_OFFSET_X, width + IMAGE_OFFSET_Y + TEXT_OFFSET))
+# На самом деле TEXT_OFFSET должено быть переменной
+result_image.paste(background, (IMAGE_OFFSET_X, IMAGE_OFFSET_Y))
+result_image.show()
