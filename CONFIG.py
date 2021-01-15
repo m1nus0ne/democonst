389b412c4c543615b4d71b53cf55f8107e8797c2
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2021-01-15 14:02:02 +0500

    update alg

--- CONFIG.py
+++ CONFIG.py
@@ -3,13 +3,15 @@ from PIL import ImageFont
 BORDER = 20
 IMAGE_OFFSET_X = 68
 IMAGE_OFFSET_Y = 37
-TEXT_OFFSET = 200
+HEAD_TEXT = 'NIGGERS '* 17
+PLAIN_TEXT = 'White power ' * 20
 LINE_SIZE = 4
 HEAD_FONT = ImageFont.truetype('font.ttf', encoding='unic', size=40)  # 24pic ширина 28pic высота
 PLAIN_FONT = ImageFont.truetype('font.ttf', encoding='unic', size=28)  # 17pic ширина 20pic  высота
 HEAD_SHIFT = 28
 PLAIN_SHIFT = 20
-
+TEXT_OFFSET = 300
+Y_PLAIN = 0
 
 # COLORS
 WHITE = (255, 255, 255)  # POWER
--- const.py
+++ const.py
@@ -5,30 +5,39 @@ from time import sleep
 from CONFIG import *  # Ты шизоид?
 from textwrap import wrap
 
+
+
+
+
+
+
+
 # Часто работаем с кортежами, пишем нужную функцию округления
 def rround(*args):
 	return tuple(map(round, args))
+
 def textprint(text,y_cord: int,font:ImageFont):
 	'''В блядском PIL нет функции ввода в область, пишем ее сами
 		Выравнивание по центру'''
+	global Y_PLAIN
 	if font.size == 40:
 		text = wrap(text,HEAD_LIMIT)
 		for line in text:
-			x_cord = (result_image.width-len(line)*24)/2
+			x_cord = (result_image.width-len(line)*24)/2 # Оп центруем
 			draw = ImageDraw.Draw(result_image)
 			draw.text(rround(x_cord,y_cord),line, WHITE, HEAD_FONT)
 
 			y_cord+=HEAD_SHIFT*1.5
 	else:
-		text = wrap(text,PLAIN_LIMIT)
+		text = wrap(text, PLAIN_LIMIT)
 		for line in text:
-			draw = ImageDraw.Draw()
-			draw.text(cord,line,WHITE,PLAIN_FONT)
-			cord = rround(cord[0], cord[1] + 10 * PLAIN_SHIFT)
+			x_cord = (result_image.width - len(line) * 17) / 2
+			draw = ImageDraw.Draw(result_image)
+			draw.text(rround(x_cord, y_cord), line, WHITE, PLAIN_FONT)
 
+			y_cord += PLAIN_SHIFT * 1.5
 
-
-
+	Y_PLAIN = y_cord
 
 
 
@@ -70,7 +79,9 @@ PLAIN_LIMIT = (background.width - IMAGE_OFFSET_X)//17
 # Добавляем текст по классике: ЗАГОЛОВОК текст, маштабируем поле ввода вниз, ширина по консту
 # Отцентровка расчитываеться отдельно
 draw = ImageDraw.Draw(result_image)
-textprint('NIGGERS '* 10,background.height+IMAGE_OFFSET_Y,HEAD_FONT)
+textprint(HEAD_TEXT,background.height+IMAGE_OFFSET_Y,HEAD_FONT)
+textprint(PLAIN_TEXT,Y_PLAIN,PLAIN_FONT)
 result_image.show()
 print((width, width))
 print(result_image.size)
+print(Y_PLAIN)
